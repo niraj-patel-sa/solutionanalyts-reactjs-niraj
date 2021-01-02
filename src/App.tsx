a5d9e11e5392f8fc6cb38f8d69ca82a792858c90
Niraj Patel <niraj.patel@solutionanalysts.com>
2021-01-02 10:03:33 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <section>
+        <span>Welcome to Leaderboard</span>
+      </section>
+    </div>
+  );
+}
+
+export default App;
